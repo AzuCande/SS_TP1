cf730575811792abbca69d72f3dfb4142f82e4c5
malevasquez <male.vasquez@yahoo.com.ar>
2023-03-08 23:17:14 -0300

    Missed file from last commit

--- src/main/java/ar/edu/itba/ss/tp1/Parser.java
+++ src/main/java/ar/edu/itba/ss/tp1/Parser.java
@@ -1,37 +1,37 @@
-//package ar.edu.itba.ss.tp1;
-//
-//import java.util.Properties;
-//
-//public class Parser {
-//
-//    private String PERIODIC = "periodic";
-//
-//    /**
-//     * To add a parameter to your runner, add a VM option in the configuration with the folowing format:
-//     * -Dparameter=value
-//    */
-//    public void parse() {
-//        Properties properties = System.getProperties();
-//        String periodic = properties.getProperty(PERIODIC);
-//
-////        System.out.println("properties: " + properties);
-//        System.out.println("periodic: " + periodic);
-//
-//        if (periodic == null) {
-//            System.err.println("No periodic parameter provided");
-//            System.exit(1);
-//        }
-//
-//        switch (periodic) {
-//            case "true":
-//                Constants.setPeriodic(true);
-//                break;
-//            case "false":
-//                break;
-//            default:
-//                System.err.println("Invalid value for periodic parameter");
-//                System.exit(1);
-//        }
-//
-//    }
-//}
+package ar.edu.itba.ss.tp1;
+
+import java.util.Properties;
+
+public class Parser {
+
+    private String PERIODIC = "periodic";
+
+    /**
+     * To add a parameter to your runner, add a VM option in the configuration with the folowing format:
+     * -Dparameter=value
+    */
+    public void parse() {
+        Properties properties = System.getProperties();
+        String periodic = properties.getProperty(PERIODIC);
+
+//        System.out.println("properties: " + properties);
+        System.out.println("periodic: " + periodic);
+
+        if (periodic == null) {
+            System.err.println("No periodic parameter provided");
+            System.exit(1);
+        }
+
+        switch (periodic) {
+            case "true":
+                Constants.setPeriodic(true);
+                break;
+            case "false":
+                break;
+            default:
+                System.err.println("Invalid value for periodic parameter");
+                System.exit(1);
+        }
+
+    }
+}
